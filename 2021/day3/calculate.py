4a62ad4a929f1f6736843460a5496ddfef2d99b7
Marc Brooks <marcwbrooks@gmail.com>
2021-12-03 09:22:10 -0500

    Day 3 done

--- 2021/day3/calculate.py
+++ 2021/day3/calculate.py
@@ -1,19 +1,64 @@
 import click
 import tqdm
 
+
+def calc_rating(ones_count, readings, spot, comp_func):
+
+    # print(f"{ones_count=}")
+    # print(f"{readings=}")
+    # print(f"{spot=}")
+    if len(readings) == 1:
+        return readings
+    else:
+        num_readings = len(readings)
+        one_count = ones_count[spot]
+        zero_count = num_readings - ones_count[spot]
+        value_to_use = comp_func(one_count, zero_count) 
+        # print(f"{num_readings=}")
+        # print(f"{one_count=}")
+        # print(f"{zero_count=}")
+        # print(f"{value_to_use=}")
+        # print("*******************")
+        filtered_readings = []
+        for reading in readings:
+            if reading[spot] == value_to_use:
+                filtered_readings.append(reading)
+
+        return calc_rating(count_ones(filtered_readings), filtered_readings, spot+1, comp_func)
+
+def ox_comp_func(one_count, zero_count):
+    if one_count >= zero_count:
+        return '1'
+    else:
+        return '0'
+
 def calc_ox_rating(ones_count, readings):
+    start_spot = 0
+    ox_rating = calc_rating(ones_count, readings, start_spot, ox_comp_func)
+    return ox_rating
 
-    num_readings = len(readings)
+def co2_comp_func(one_count, zero_count):
+    if zero_count <= one_count:
+        return '0'
+    else:
+        return '1'
 
-    for spot, one_count in enumerate(ones_count):
-        zero_count = num_readings - one_count
-        print(f"{zero_count=}")
-        print(f"{one_count=}")
-        print(f"{num_readings=}")
+def calc_co2_rating(ones_count, readings):
+    # print("STARTING CO2 RATING CALC")
 
-def calc_c02_rating(ones_count, readings):
+    start_spot = 0
+    co2_rating = calc_rating(ones_count, readings, start_spot, co2_comp_func)
+    return co2_rating
 
-    pass 
+def count_ones(readings):
+    ones_count = [0] * (len(readings[0])-1)
+
+    for reading in readings:
+        for spot, value in enumerate(reading.strip()):
+            if value == '1':
+                ones_count[spot] += 1
+
+    return ones_count
 
 def process_readings(readings):
 
@@ -21,12 +66,7 @@ def process_readings(readings):
     epsilon_rate = 0
     num_readings = len(readings)
 
-    ones_count = [0] * (len(readings[0])-1)
-
-    for reading in readings:
-        for spot, value in enumerate(reading.strip()):
-            if value == '1':
-                ones_count[spot] += 1
+    ones_count = count_ones(readings)
 
     gamma_rate = []
     epsilon_rate = []
@@ -43,15 +83,15 @@ def process_readings(readings):
 
     gamma_rate = int('0b' + ''.join(gamma_rate), 2)
     epsilon_rate = int('0b' + ''.join(epsilon_rate), 2)
-    print(ones_count)
+    # print(ones_count)
 
-    ox_gen_rating = calc_ox_rating(ones_count, readings)
-    co2_gen_rating = calc_c02_rating(ones_count, readings)
+    ox_gen_rating = int('0b' + calc_ox_rating(ones_count, readings)[0], 2)
+    co2_gen_rating = int('0b' + calc_co2_rating(ones_count, readings)[0], 2)
     rates = {
         "gamma_rate": gamma_rate,
         "epsilon_rate": epsilon_rate,
         "ox_gen_rating": ox_gen_rating,
-        "c02_gen_rating": co2_gen_rating,
+        "co2_gen_rating": co2_gen_rating,
     }
     return rates
 
@@ -60,15 +100,19 @@ def process_readings(readings):
 def plot(inputfile):
 
     readings = inputfile.readlines()
+    # readings = [reading.strip() for reading in readings]
 
     print(f"Found {len(readings)} reading(s).")
 
     rates = process_readings(readings)
 
-    print(f'{rates["gamma_rate"]=}')
-    print(f'{rates["epsilon_rate"]=}')
+    print(f"{rates=}")
+    # print(f'{rates["gamma_rate"]=}')
+    # print(f'{rates["epsilon_rate"]=}')
     power_consumption = rates["gamma_rate"] * rates["epsilon_rate"]
+    life_support = rates["ox_gen_rating"] * rates["co2_gen_rating"]
     print(f"{power_consumption=}")
+    print(f"{life_support=}")
 
 
 if __name__ == "__main__":

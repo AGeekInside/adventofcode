c1a96830476824fb5d0f3d78b431334bef0ee3e5
Marc Brooks <marcwbrooks@gmail.com>
2021-12-09 22:25:57 -0500

    day 9 working

--- 2021/day9/driver.py
+++ 2021/day9/driver.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 
 import click
+from icecream import ic
 
 
 def read_map(inputfile):
@@ -21,48 +22,85 @@ def read_map(inputfile):
 
 def find_risk_levels(heightmap):
 
+    low_points = []
     risk_levels = [] 
     for i, row in enumerate(heightmap["map"]):
         # print(f"{row=}")
         for j, value in enumerate(row):
             is_lowest = True
-            # print(f"{i=}")
-            # print(f"{j=}")
-            # print(f"{value=}")
             if j > 0:
-                # print(f"Checking if {row[j-1]} <= {value}")
                 if row[j-1] <= value:
                     is_lowest = False
             if j < heightmap["width"] - 1:
-                # print(f"Checking if {row[j+1]} <= {value}")
                 if row[j+1] <= value:
                     is_lowest = False
             if i > 0:
-                # print(f"Checking if {heightmap['map'][i-1][j]} <= {value}")
                 if heightmap["map"][i-1][j] <= value:
                     is_lowest = False
             if i < heightmap["depth"] - 1:
-                # print(f"Checking if {heightmap['map'][i+1][j]} <= {value}")
                 if heightmap["map"][i+1][j] <= value:
                     is_lowest = False
             if is_lowest:
                 risk_levels.append(value+1)
+                low_points.append((i, j))
 
+    heightmap["low_points"] = low_points
+    heightmap["risk_levels"] = risk_levels
     return risk_levels
             
 
+def find_basin(location, current_basin, heightmap):
+    row = location[0]
+    column = location[1]
+    location_value = heightmap["map"][row][column]
 
+    if location in current_basin:
+        return
+    if location_value == 9:
+        return
+    else:
+        current_basin.append(location)
+        if column > 0:
+            find_basin((row, column-1), current_basin, heightmap)
+        if column < heightmap["width"] - 1:
+            find_basin((row, column+1), current_basin, heightmap)
+        if row > 0:
+            find_basin((row-1, column), current_basin, heightmap)
+        if row < heightmap["depth"] - 1:
+            find_basin((row+1, column), current_basin, heightmap)
+
+
+def find_basins(heightmap):
+
+    chasms = []
+
+    for low_point in heightmap["low_points"]:
+        current_basin = []
+        find_basin(low_point, current_basin, heightmap)
+        chasms.append({
+            "basin": current_basin,
+            "size": len(current_basin),
+        })
+
+    sorted_chasms = sorted(chasms, key = lambda i: i['size'], reverse=True)
+    return sorted_chasms
+    
 
 @click.command()
 @click.argument('inputfile', type=click.File('r'))
 def driver(inputfile):
     heightmap = read_map(inputfile)
-    # pprint(heightmap)
 
     risk_levels = find_risk_levels(heightmap)
-    # pprint(risk_levels)
 
     print(f"Sum of risk: {sum(risk_levels)}")
+
+    basins = find_basins(heightmap)
+
+    answer = basins[0]["size"] * basins[1]["size"] * basins[2]["size"] 
+
+    ic(answer)
+
 
 if __name__ == "__main__":
     driver()

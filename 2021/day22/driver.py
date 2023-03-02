6ac9b83659ccb148769007d6fc7cf97aa3b8e692
Marc Brooks <marcwbrooks@gmail.com>
2023-03-02 08:13:34 -0500

    adding some files

--- 2021/day22/driver.py
+++ 2021/day22/driver.py
@@ -1,9 +1,16 @@
 import click
 from icecream import ic
 
+class ReactorCore:
+    
+    def __init__(self, min=-50, max=50):
+        self.min = min
+        self.max = max
+
 @click.command()
 @click.argument('inputfile', type=click.File('r'))
 def driver(inputfile):
+
 
     pass
 

dee10605c167bc87da285a01fc128e2a0c7d5170
Marc Brooks <marcwbrooks@gmail.com>
2023-03-02 09:35:35 -0500

    setting up directories

--- 2022/setup_directories.py
+++ 2022/setup_directories.py
@@ -8,8 +8,19 @@ def main():
         print(directory)
 
         # create the directory
-        os.mkdir(directory)
-    
+        #os.mkdir(directory)
+
+        # copy driver.py to the directory and rename it to main.py
+        os.system(f"cp driver.py {directory}/{directory}.py")
+
+        # copy input.txt to the directory
+        os.system(f"touch {directory}/input.txt")
+
+        # copy test_input.txt to the directory
+        os.system(f"touch {directory}/test_input.txt")
+
+        # copy test_output.txt to the directory
+        os.system(f"touch {directory}/test_output.txt")
 
 if __name__ == "__main__":
     main()
